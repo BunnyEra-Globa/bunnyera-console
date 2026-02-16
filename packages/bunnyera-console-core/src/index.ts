6b424f380c06171b4ca960638f9e69ae40e1624c
BunnyEra-Global <bunnyera.founder@gmail.com>
2026-02-17 02:07:29 +0800

    Create index.ts

---
+++ packages/bunnyera-console-core/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./system/systemInfo";
+export * from "./logs/logService";
+export * from "./config/configService";
+export * from "./plugins/pluginManager";
+export * from "./ai/aiRuntime";
+export * from "./fs/fileService";
+export * from "./ipc/ipcHandlers";
